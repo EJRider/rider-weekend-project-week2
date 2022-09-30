0f08fe6e958f20393c79da2763622c7896fde512
Rider <goejrider@gmail.com>
2022-09-30 09:55:38 -0500

    Add app.post to recieve math and save math on server side

--- server/public/client.js
+++ server/public/client.js
@@ -6,6 +6,8 @@ let givenValue;
 
 let prepToSend;
 
+let numberOfProblems = -1;
+
 function onReady(){
     console.log('jquery running');
     //an onClick that only calls buttons with the mathButtons class without submitting
@@ -15,6 +17,11 @@ function onReady(){
     $('#mathForm').on('submit', submitMath);
 }
 
+
+// this function takes in both number inputs and the chosen math equation, then sends 
+// that information to the server.
+// it also checks if the value they sent in was C, if that is true it will clear
+// the inputs and end the function.
 function getValue(){
     console.log('getting value of ', this);
     givenValue = $(this).val();
@@ -31,10 +38,25 @@ function getValue(){
         chosenEquation: givenValue
     }
 }
+    $('#firstNumber').val();
+    $('#secondNumber').val();
     console.log(prepToSend);
 }
 
+
+// this function takes the prepToSend object and sends it over to the server.
 function submitMath(evt){
     evt.preventDefault();
     console.log('submitting math to server');
+    $.ajax({
+        url:'/math-solve',
+        method: 'POST',
+        data: prepToSend
+    })
+        .then(response => {
+            console.log('data recieved', response);
+        })
+        .catch(err => {
+            console.log('there was an error in submit math', err);
+        })
 }
--- server/server.js
+++ server/server.js
@@ -2,9 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express();
 
+let mathHistory = [];
+
+let numberOfProblemsSolved = -1;
+
 app.use(express.static('./server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(5000, ()=>{
     console.log('Server is listening');
 });
+
+app.post('/math-solve', (req,res)=>{
+    let newProblem = req.body;
+    mathHistory.push(newProblem);
+    console.log(mathHistory);
+    res.sendStatus(201);
+})
